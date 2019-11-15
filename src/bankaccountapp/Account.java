ad19393ad3220204cdac67cae6f24c10dc35d77b
patrickdang <patrickdang6356@gmail.com>
2019-11-15 17:10:45 -0600

    clean up comments

--- src/bankaccountapp/Account.java
+++ src/bankaccountapp/Account.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bankaccountapp;
 
-/**
- *
- * @author Patrick
- */
 public abstract class Account implements IBaseRate {
     //List common properties for savings and checking accounts
     private String name;
