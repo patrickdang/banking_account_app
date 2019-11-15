e8a62b366dfa59caff04b95c2c5740172b498afb
patrickdang <patrickdang6356@gmail.com>
2019-11-15 17:13:01 -0600

    clean up comments

--- src/utilities/CSV.java
+++ src/utilities/CSV.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package utilities;
 
 import java.util.LinkedList;
@@ -12,10 +7,7 @@ import java.io.FileReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-/**
- *
- * @author Patrick
- */
+
 public class CSV {
     
     //This function will read data from a CSV file and return as a list
