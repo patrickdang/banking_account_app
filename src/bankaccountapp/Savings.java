7fe93a97b1c086073e9a56d18fe41657a20f2131
patrickdang <patrickdang6356@gmail.com>
2019-11-15 17:12:24 -0600

    clean up comments

--- src/bankaccountapp/Savings.java
+++ src/bankaccountapp/Savings.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bankaccountapp;
 
-/**
- *
- * @author Patrick
- */
+
 public class Savings extends Account{
     //List properties specific to the Savings account
     private int safetyDepositBoxID;
